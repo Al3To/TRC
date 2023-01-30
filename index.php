9fb9ab48ed99659e31f2fc187f8d055bd9dd93c1
Al3To <aletommy238@gmail.com>
2023-01-30 09:57:08 +0100

    Added a simple navbar and made the project base

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Tommasi Restaurant Corporation</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+    <nav class="autohide navbar fixed-top navbar-expand-xxl navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">
+                    <img src="./img/logo.png" height="36">
+                </a>
+            </div>
+            <div class="btn-group">
+                <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Utente</button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Login</a>
+                    <a class="dropdown-item" href="#">Registrati</a>
+                </div>
+            </div>
+    
+        </div>
+    </nav>
+
+
+<script src="scriptNavbar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ scriptNavbar.js
@@ -0,0 +1,19 @@
+document-addEventListener("DOMContentLoaded", function(){
+
+        autohide = document.querySelector('.autohide');
+        if(autohide){
+            var last_scroll_top = 0;
+            window.addEventListener('scroll', function(){
+                let scroll_top = window.scrollY;
+                if(scroll_top < last_scroll_top){
+                    autohide.classList.remove('scrolled-down');
+                    autohide.classList.add('scrolled-up');
+                }
+                else {
+                    autohide.classList.remove('scrolled-up');
+                    autohide.classList.add('scrolled-down');
+                }
+                last_scroll_top = scroll_top;
+            })
+        }
+});
